7d73c8a4cbd218f430ef7a107af7b4578b5822e3
Eduardo Menezes <drummeduardo@gmail.com>
2025-10-25 12:24:57 -0300

    Atualizando o VGG16
    Garantir funcionamento com o tam de imagens que estamos usando

--- src/models/vgg16.py
+++ src/models/vgg16.py
@@ -8,29 +8,28 @@
 import torch
 import torch.nn as nn
 
-#implementation
-
+# implementation
 class VGG16(nn.Module):
     def __init__(self, num_classes=200):
         super(VGG16, self).__init__()
         
-        # feature extractor
+        # Base convolucional (feature extractor)
         self.features = nn.Sequential(
-            # first block
+            # Bloco 1
             nn.Conv2d(3, 64, kernel_size=3, padding=1),
             nn.ReLU(inplace=True),
             nn.Conv2d(64, 64, kernel_size=3, padding=1),
             nn.ReLU(inplace=True),
             nn.MaxPool2d(kernel_size=2, stride=2),
             
-            # second block
+            # Bloco 2
             nn.Conv2d(64, 128, kernel_size=3, padding=1),
             nn.ReLU(inplace=True),
             nn.Conv2d(128, 128, kernel_size=3, padding=1),
             nn.ReLU(inplace=True),
             nn.MaxPool2d(kernel_size=2, stride=2),
             
-            # third block
+            # Bloco 3
             nn.Conv2d(128, 256, kernel_size=3, padding=1),
             nn.ReLU(inplace=True),
             nn.Conv2d(256, 256, kernel_size=3, padding=1),
@@ -39,7 +38,7 @@ class VGG16(nn.Module):
             nn.ReLU(inplace=True),
             nn.MaxPool2d(kernel_size=2, stride=2),
             
-            # fourth block
+            # Bloco 4
             nn.Conv2d(256, 512, kernel_size=3, padding=1),
             nn.ReLU(inplace=True),
             nn.Conv2d(512, 512, kernel_size=3, padding=1),
@@ -48,7 +47,7 @@ class VGG16(nn.Module):
             nn.ReLU(inplace=True),
             nn.MaxPool2d(kernel_size=2, stride=2),
             
-            # fifth block
+            # Bloco 5
             nn.Conv2d(512, 512, kernel_size=3, padding=1),
             nn.ReLU(inplace=True),
             nn.Conv2d(512, 512, kernel_size=3, padding=1),
@@ -58,9 +57,12 @@ class VGG16(nn.Module):
             nn.MaxPool2d(kernel_size=2, stride=2),
         )
         
-        # classifier
+        # Adiciona uma camada de pooling adaptativo para garantir a saída 7x7
+        self.avgpool = nn.AdaptiveAvgPool2d((7, 7))
+        
+        # Classificador corrigido para aceitar a saída 512*7*7
         self.classifier = nn.Sequential(
-            nn.Linear(512 * 2 * 2, 4096),  # 64x64 input(tinyImagenet)
+            nn.Linear(512 * 7 * 7, 4096),
             nn.ReLU(inplace=True),
             nn.Dropout(),
             nn.Linear(4096, 4096),
@@ -70,9 +72,16 @@ class VGG16(nn.Module):
         )
     
     def forward(self, x):
+        # Passa pela base convolucional
         x = self.features(x)
         
+        # Aplica o pooling adaptativo
+        x = self.avgpool(x)
+        
+        # Achata o tensor para o classificador
         x = torch.flatten(x, 1)  
         
+        # Passa pelo classificador
         x = self.classifier(x)
         return x
+
