6ebe547f787449505309373a0366c5ca6c656abc
Eduardo Menezes <drummeduardo@gmail.com>
2025-11-04 19:15:32 -0300

    Update channels.scm

--- channels.scm
+++ channels.scm
@@ -4,12 +4,4 @@
     (url "https://git.savannah.gnu.org/git/guix.git")
     (branch "master")
     (commit "a729c88f6702478ab2405bc719cb74b14d13f387")
-    )
-  (channel
-    (name 'guix-science-nonfree)
-    (url "https://codeberg.org/guix-science/guix-science-nonfree.git")
-    (introduction
-      (make-channel-introduction
-        "58661b110325fd5d9b40e6f0177cc486a615817e"
-        (openpgp-fingerprint
-          "CA4F 8CF4 37D7 478F DA05 5FD4 4213 7701 1A37 8446")))))
+    ))
