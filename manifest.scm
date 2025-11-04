6ef0a66b49c1156312e36b9dc99ee155e861c306
Eduardo Menezes <drummeduardo@gmail.com>
2025-11-04 20:32:16 -0300

    Update manifest.scm

--- manifest.scm
+++ manifest.scm
@@ -5,7 +5,7 @@
    ;; Python + ferramentas para instalar via pip no ambiente puro
    "bash-minimal"
    "python"
-   "python@3.10"
+   "python-wrapper"
    "python-pip"
    "python-setuptools"
    "python-wheel"
