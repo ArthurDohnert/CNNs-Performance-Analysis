e13f76f9f969dd5fb07c122d2d511f97b8e8ba84
Eduardo Menezes <drummeduardo@gmail.com>
2025-11-06 17:42:29 -0300

    Guix  update

--- manifest-gpu.scm
+++ manifest-gpu.scm
@@ -1,27 +1,27 @@
 ;; --- manifest-gpu.scm ---
-(use-modules
-  (guix packages)
-  (guix profiles)
-  (gnu packages python-science)
-  (gnu packages python-xyz)
-  (gnu packages graphics)
-  (gnu packages machine-learning)
-  (gnu packages maths)
-  (my-local-packages timm))
+(use-modules (guix profiles)
+             (guix packages)
+             (gnu packages python)
+             (gnu packages python-science)
+             (gnu packages python-xyz)
+             (gnu packages graphics)
+             (gnu packages machine-learning)
+             (gnu packages maths)
+             (my-local-packages timm))
 
 (specifications->manifest
- (list
-  "python"
-  "python-psutil"
-  "python-nvidia-ml-py"
-  "python-pytorch-with-cuda11"
-  "python-timm"
-  "python-numpy"
-  "python-pandas"
-  "python-scikit-learn"
-  "python-matplotlib"
-  "python-seaborn"
-  "python-tqdm"
-  "python-pillow"
-  "python-pyyaml"
-  "python-tabulate"))
+  (list
+   "python"
+   "python-psutil"
+   "python-nvidia-ml-py"
+   "python-pytorch-with-cuda11"
+   "python-timm"
+   "python-numpy"
+   "python-pandas"
+   "python-scikit-learn"
+   "python-matplotlib"
+   "python-seaborn"
+   "python-tqdm"
+   "python-pillow"
+   "python-pyyaml"
+   "python-tabulate"))
