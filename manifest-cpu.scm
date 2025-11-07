01787828ce8af0cd5a14555584bf027f89611ef7
Eduardo Menezes <drummeduardo@gmail.com>
2025-11-07 15:40:26 -0300

    Update manifest-cpu.scm

--- manifest-cpu.scm
+++ manifest-cpu.scm
@@ -1,26 +1,26 @@
 ;; --- manifest-cpu.scm ---
-#:use-module (guix packages)
-#:use-module (gnu packages python-science)
-#:use-module (gnu packages python-xyz)
-#:use-module (gnu packages graphics)
-#:use-module (gnu packages machine-learning)
-#:use-module (gnu packages maths)
-#:use-module (gnu packages text-utils)
-#:use-module (my-local-packages timm)
+(use-modules (guix profiles)
+             (guix packages)
+             (gnu packages python)
+             (gnu packages python-science)
+             (gnu packages python-xyz)
+             (gnu packages graphics)
+             (gnu packages machine-learning)
+             (gnu packages maths)
+             (timm)
+             (huggingface-hub))
 
-(specifications->manifest
-  (list
-   "python"
-   "python-psutil"
-   "python-pytorch" 
-   "python-timm"    
-   "python-numpy"
-   "python-pandas"
-   "python-scikit-learn"
-   "python-matplotlib"
-   "python-seaborn"
-   "python-tqdm"
-   "python-pillow"
-   "python-pyyaml"
-   "python-tabulate"
-   ))
+(packages->manifest
+ (list python
+       python-psutil
+       python-pytorch
+       python-timm
+       python-numpy
+       python-pandas
+       python-scikit-learn
+       python-matplotlib
+       python-seaborn
+       python-tqdm
+       python-pillow
+       python-pyyaml
+       python-tabulate))
